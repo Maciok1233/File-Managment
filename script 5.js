a281025a474b8850804c7c5297f25af69209ea05
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-26 16:30:42 +0200

    Delete script 5.js

--- script 5.js
+++
@@ -1,4 +0,0 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  document.getElementById("header1").style.height = "140px";
-}
