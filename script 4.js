321284c8262bee69bbe79d6d7eb6f5400b9a4433
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-26 16:30:34 +0200

    Delete script 4.js

--- script 4.js
+++
@@ -1,9 +0,0 @@
-let user = getCookie("username");
-if (user != "") {
-  let element0 = document.getElementById('image');
-   element0.ondblclick();
-   document.getElementById("div").style.animationPlayState = "running";
-   document.getElementById("div").style.pointerEvents = "auto";
-   document.getElementById("div2").style.pointerEvents = "none";
-   Test2 = 1;
-}
