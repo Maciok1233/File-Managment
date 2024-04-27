6c04fa01e3bfc391d31584afd40bb6ef3e3b3306
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-27 13:21:35 +0200

    Update script 5.js

--- script 5.js
+++ script 5.js
@@ -15,5 +15,6 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   document.getElementById("LOG-OUT").style.fontSize = "13px";
   document.getElementById("LOG-OUT").style.top = "2px";
   document.getElementById("image").style.height = "34px";
-  document.getElementsById('box').style.width = "200px";
+  const boxes = document.querySelectorAll('#box');
+  for (let i = 0; i < boxes.length; i++) { boxes[i].style.width = '200px'; }
 }
