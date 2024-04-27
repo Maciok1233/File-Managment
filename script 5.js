ba601553fd0e85706f8236645328e778d678a67b
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-27 13:12:00 +0200

    Update script 5.js

--- script 5.js
+++ script 5.js
@@ -15,5 +15,5 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   document.getElementById("LOG-OUT").style.fontSize = "13px";
   document.getElementById("LOG-OUT").style.top = "2px";
   document.getElementById("image").style.height = "34px";
-  document.getElementById("box").style.width = "40%";
+  document.getElementById("box").style.width = "200px";
 }
