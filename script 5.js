4572e8a6e813d4dfc066f90c6cc8f9f2e97913b1
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-27 14:20:49 +0200

    Update script 5.js

--- script 5.js
+++ script 5.js
@@ -10,7 +10,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   document.getElementById("CONTACT").style.position = "absolute";
   document.getElementById("HOME").innerText = "";
   document.getElementById("HOME").style.position = "absolute";
-  document.getElementById("searchbar").setAttribute("size", "2");
+  document.getElementById("searchbar").setAttribute("size", "1.5");
   document.getElementById("phone-adjustment").style.height = "20px";
   document.getElementById("LOG-OUT").style.fontSize = "13px";
   document.getElementById("LOG-OUT").style.top = "2px";
