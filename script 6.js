4a24961588fd4f418b7e3df8bb8a9482471bf3fb
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-28 14:03:40 +0200

    Update script 6.js

--- script 6.js
+++ script 6.js
@@ -5,17 +5,9 @@ let screenHeight = window.innerHeight;
 if (screenWidth < 1501 || screenHeight < 801) {
   document.getElementById("Mark1").innerText = "";
   document.getElementById("Mark2").innerText = "";
-  document.getElementById("ABOUT").innerText = "";
-  document.getElementById("ABOUT").style.position = "absolute";
-  document.getElementById("CONTACT").innerText = "";
-  document.getElementById("CONTACT").style.position = "absolute";
  }
 else {
   document.getElementById("Mark1").innerText = "Private";
   document.getElementById("Mark2").innerText = "Private"; 
-  document.getElementById("ABOUT").innerText = "About";
-  document.getElementById("ABOUT").style.position = "fixed";
-  document.getElementById("CONTACT").innerText = "Contact";
-  document.getElementById("CONTACT").style.position = "fixed";
 }
 }, 100);
