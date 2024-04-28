caecbc034d3a965e8004384602ac91cea8ce9c53
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-28 13:58:37 +0200

    Update script 6.js

--- script 6.js
+++ script 6.js
@@ -5,6 +5,10 @@ let screenHeight = window.innerHeight;
 if (screenWidth < 1501 || screenHeight < 801) {
   document.getElementById("Mark1").innerText = "";
   document.getElementById("Mark2").innerText = "";
+  document.getElementById("ABOUT").innerText = "";
+  document.getElementById("ABOUT").style.position = "absolute";
+  document.getElementById("CONTACT").innerText = "";
+  document.getElementById("CONTACT").style.position = "absolute";
  }
 else {
   document.getElementById("Mark1").innerText = "Private";
