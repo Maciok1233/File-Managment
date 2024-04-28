d31246d98d210dc70e234e8cbed5aebdd770fa3a
Maciok1233 <105968753+Maciok1233@users.noreply.github.com>
2024-04-28 13:53:22 +0200

    Update script 6.js

--- script 6.js
+++ script 6.js
@@ -6,4 +6,8 @@ if (screenWidth < 1501 || screenHeight < 801) {
   document.getElementById("Mark1").innerText = "";
   document.getElementById("Mark2").innerText = "";
  }
+else {
+  document.getElementById("Mark1").innerText = "Private";
+  document.getElementById("Mark2").innerText = "Private"; 
+}
 }, 100);
